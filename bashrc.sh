d1986556afe4106d4af51792128ef893c9b2ae2f
André Bueno <andre_devops@hotmail.com>
2025-05-14 10:50:32 -0300

    update_bashrc

--- bashrc.sh
+++ bashrc.sh
@@ -121,3 +121,4 @@ if ! shopt -oq posix; then
 fi
 
 complete -C /usr/bin/terraform terraform
+export PATH=$PATH:"/c/Users/abusilva/AppData/Local/Packages/PythonSoftwareFoundation.Python.3.11_qbz5n2kfra8p0/LocalCache/local-packages/Python311/Scripts"
