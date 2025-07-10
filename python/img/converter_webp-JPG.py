e5ec2f9aaa26585581cfa1833876909026a05c12
André Bueno <andre_devops@hotmail.com>
2025-07-10 14:46:08 -0300

    img

--- python/img/converter_webp-JPG.py
+++
@@ -1,9 +0,0 @@
-from PIL import Image
-
-# Abra a imagem WEBP
-webp_image = Image.open('rebel.webp')
-
-# Converta para JPG
-webp_image.convert('RGB').save('imagem_convertida.jpg', 'JPEG')
-
-print("Conversão concluída com sucesso!")
